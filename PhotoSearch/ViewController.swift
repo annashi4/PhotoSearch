ca1534bf7990466bda26b9f2b92026ada5ae977c
annashi4 <ann.shirinkina@gmail.com>
2022-06-27 23:41:09 +0400

    Initial Commit

---
+++ PhotoSearch/ViewController.swift
@@ -0,0 +1,80 @@
+import UIKit
+
+struct APIResponse: Codable {
+    let total: Int
+    let total_pages: Int
+    let results: [Results]
+    
+}
+
+struct Results: Codable {
+    let id: String
+    let urls: URLS
+}
+
+struct URLS: Codable {
+    let full: String
+}
+
+class ViewController: UIViewController, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return results.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell",
+                                                      for: indexPath)
+        cell.backgroundColor = .blue
+        return cell
+    }
+    
+    
+    let url: String = "https://api.unsplash.com/search/photos?page=1&query=office&client_id=lMWX_l3h0M2_t0BNhysonSouKZ3z232UP7LoG7sArnw"
+    
+    private var collectionView: UICollectionView?
+    
+    var results: [Results] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
+        layout.itemSize = CGSize(width: view.frame.size.width/2, height: view.frame.size.width/2)
+        let collectionView = UICollectionView(frame: .zero,
+                                              collectionViewLayout: layout)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.dataSource = self
+        view.addSubview(collectionView)
+        self.collectionView = collectionView
+        fetchPhotos()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView?.frame = view.bounds
+    }
+    
+    func fetchPhotos() {
+        guard let url =  URL(string: url) else { return }
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
+            guard let data = data, error == nil else { return }
+            
+            do{
+                let jsonResult = try JSONDecoder().decode(APIResponse.self, from: data)
+                DispatchQueue.main.async {
+                    self?.results = jsonResult.results
+                    self?.collectionView?.reloadData()
+                }
+            }catch{
+                print(error)
+            }
+        }
+        task.resume()
+        
+    }
+
+
+}
+
