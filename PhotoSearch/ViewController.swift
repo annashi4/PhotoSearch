abf128aee5975025421a0009861e3514b2036fe8
annashi4 <ann.shirinkina@gmail.com>
2022-06-28 11:49:58 +0400

    adding search option

--- PhotoSearch/ViewController.swift
+++ PhotoSearch/ViewController.swift
@@ -13,16 +13,16 @@ struct Results: Codable {
 }
 
 struct URLS: Codable {
-    let full: String
+    let regular: String
 }
 
-class ViewController: UIViewController, UICollectionViewDataSource {
+class ViewController: UIViewController, UICollectionViewDataSource, UISearchBarDelegate {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return results.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let imageURLString = results[indexPath.row].urls.full
+        let imageURLString = results[indexPath.row].urls.regular
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ImageCollectionViewCell.idenifier, for: indexPath) as? ImageCollectionViewCell else {
             return UICollectionViewCell()
         }
@@ -30,15 +30,16 @@ class ViewController: UIViewController, UICollectionViewDataSource {
         return cell
     }
     
-    
-    let url: String = "https://api.unsplash.com/search/photos?page=1&query=office&client_id=lMWX_l3h0M2_t0BNhysonSouKZ3z232UP7LoG7sArnw"
-    
     private var collectionView: UICollectionView?
     
     var results: [Results] = []
+    
+    let searchBar = UISearchBar()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchBar.delegate = self
+        view.addSubview(searchBar)
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
         layout.minimumLineSpacing = 0
@@ -50,15 +51,27 @@ class ViewController: UIViewController, UICollectionViewDataSource {
         collectionView.dataSource = self
         view.addSubview(collectionView)
         self.collectionView = collectionView
-        fetchPhotos()
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        collectionView?.frame = view.bounds
+        searchBar.frame = CGRect(x: 11, y: view.safeAreaInsets.top, width: view.frame.size.width-22, height: 22)
+        collectionView?.frame = CGRect(x: 0, y: view.safeAreaInsets.top+55, width: view.frame.size.width, height: view.frame.size.height-55)
     }
     
-    func fetchPhotos() {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        if let text = searchBar.text {
+            results = []
+            collectionView?.reloadData()
+            fetchPhotos(query: text)
+        }
+    }
+    
+    func fetchPhotos(query: String) {
+        
+        let url: String = "https://api.unsplash.com/search/photos?page=1&per_page=50&query=\(query)&client_id=lMWX_l3h0M2_t0BNhysonSouKZ3z232UP7LoG7sArnw".replacingOccurrences(of: " ", with: "-")
+        
         guard let url =  URL(string: url) else { return }
         let task = URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
             guard let data = data, error == nil else { return }
